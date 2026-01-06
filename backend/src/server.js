1e0f46b9516eb7174d2657fa43a9ec2ff39a009e
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2026-01-06 15:19:17 +0530

    DB config and tools setup.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import path from "path";
 import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();  
 
 const __dirname = path.resolve()
+app.use(clerkMiddleware()) // adds auth object under the req => req.auth
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "Success" });
@@ -21,5 +24,6 @@ if(ENV.NODE_ENV === "production"){
   
 app.listen(ENV.PORT, () => {
   console.log(`Server running on port ${ENV.PORT}`);
+  connectDB();
 });
 ;
